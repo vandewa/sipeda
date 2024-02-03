654c57ee448caad85d523bf1370b14991d871e1f
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2024-02-03 10:45:59 +0700

    penambahan update kesalahan seeder user

--- database/seeders/UserDesaSeeder.php
+++ database/seeders/UserDesaSeeder.php
@@ -15,28 +15,31 @@ class UserDesaSeeder extends Seeder
      */
     public function run(): void
     {
-        $data = ComRegion::with(['root'])->where('region_level', 4)->get();
-        foreach($data as $item){
-            if($item->root->region_cd??"" != ""){
-                $a = User::create([
-                    'name'=> 'Admin Desa '. $item->region_nm,
-                    'region_kel' => $item->region_cd,
-                    'region_kec' => $item->root->region_cd,
-                    'email' => strtolower($item->region_nm."-".$item->root->region_nm."@app.com"),
-                    'password' => Hash::make('password')
-                ]);
-                $a->addRole('desa');
-            }
-        }
+        // $data = ComRegion::with(['root'])->where('region_level', 4)->get();
+        // foreach($data as $item){
+        //     if($item->root->region_cd??"" != ""){
+        //         $a = User::create([
+        //             'name'=> 'Admin Desa '. $item->region_nm,
+        //             'region_kel' => $item->region_cd,
+        //             'region_kec' => $item->root->region_cd,
+        //             'email' => strtolower($item->region_nm."-".$item->root->region_nm."@app.com"),
+        //             'password' => Hash::make('password')
+        //         ]);
+        //         $a->addRole('desa');
+        //     }
+        // }
         $data = ComRegion::with(['root'])->where('region_level', 3)->get();
         foreach($data as $item){
-            $a = User::create([
-                'name'=> 'Admin Desa '. $item->region_nm,
-                'region_kec' => $item->region_cd,
-                'email' => strtolower('admin'.'@'.$item->region_nm."id"),
-                'password' => Hash::make('password')
+            User::where('email', strtolower('admin'.'@'.$item->region_nm."id"))->update([
+                'email' => strtolower('admin'.'@'.$item->region_nm.".id")
             ]);
-            $a->addRole('kecamatan');
+            // $a = User::create([
+            //     'name'=> 'Admin Desa '. $item->region_nm,
+            //     'region_kec' => $item->region_cd,
+            //     'email' => strtolower('admin'.'@'.$item->region_nm."id"),
+            //     'password' => Hash::make('password')
+            // ]);
+            // $a->addRole('kecamatan');
         }
     }
 
