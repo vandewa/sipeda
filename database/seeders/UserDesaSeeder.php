8ca9f2aa200b33c47ca4e529940b369d0f245cba
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2024-02-03 10:38:38 +0700

    penambahan

---
+++ database/seeders/UserDesaSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ComRegion;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserDesaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = ComRegion::with(['root'])->where('region_level', 4)->get();
+        foreach($data as $item){
+            if($item->root->region_cd??"" != ""){
+                $a = User::create([
+                    'name'=> 'Admin Desa '. $item->region_nm,
+                    'region_kel' => $item->region_cd,
+                    'region_kec' => $item->root->region_cd,
+                    'email' => strtolower($item->region_nm."-".$item->root->region_nm."@app.com"),
+                    'password' => Hash::make('password')
+                ]);
+                $a->addRole('desa');
+            }
+        }
+        $data = ComRegion::with(['root'])->where('region_level', 3)->get();
+        foreach($data as $item){
+            $a = User::create([
+                'name'=> 'Admin Desa '. $item->region_nm,
+                'region_kec' => $item->region_cd,
+                'email' => strtolower('admin'.'@'.$item->region_nm."id"),
+                'password' => Hash::make('password')
+            ]);
+            $a->addRole('kecamatan');
+        }
+    }
+
+}
